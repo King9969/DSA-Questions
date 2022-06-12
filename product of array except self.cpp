d09c780152ce949a6c58aaf37931ccb35cdaff3c
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-13 00:47:18 +0530

    Update product of array except self.cpp

--- product of array except self.cpp
+++ product of array except self.cpp
@@ -10,3 +10,15 @@
            x=x*nums[i];
        }
        return v;
+//
+  vector<long long int>ans;
+       for(int i=0;i<n;i++){
+           long long int x=1;
+           for(int j=0;j<n;j++){
+               if(i!=j){
+                   x*=nums[j];
+               }
+           }
+           ans.push_back(x);
+       }
+       return ans;

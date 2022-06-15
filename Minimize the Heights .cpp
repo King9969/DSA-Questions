7623a84feabbd53d015e70ad0058f40e56c2fe1f
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-15 21:34:28 +0530

    Update and rename Minimize the Heights I .cpp to Minimize the Heights .cpp

--- Minimize the Heights .cpp
+++ Minimize the Heights .cpp
@@ -1,6 +1,7 @@
         sort(arr,arr+n);
        int mi,mx,a=arr[n-1]-arr[0];
        for(int i=1;i<n;i++){
+              //  if(arr[i] - k < 0) continue; for II
            mi=min(arr[0]+k,arr[i]-k);
            mx=max(arr[n-1]-k,arr[i-1]+k);
            a=min(a,mx-mi);

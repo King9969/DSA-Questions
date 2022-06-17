f85bfb17154070b9e6ffe0e7edf3cf2e57f26567
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-17 16:08:33 +0530

    Create Left View of Binary Tree.cpp

---
+++ Left View of Binary Tree.cpp
@@ -0,0 +1,13 @@
+   vector<int> v;
+   if(root==NULL)return v;
+   queue<Node*> q;
+   q.push(root);
+   while(q.empty()==false){
+       int n=q.size();
+       for(int i=0;i<n;i++){
+           Node *cur=q.front();q.pop();
+           if(i==0)v.push_back(cur->data);
+           if(cur->left != NULL)q.push(cur->left);
+           if(cur->right != NULL)q.push(cur->right);
+       }
+   }return v;

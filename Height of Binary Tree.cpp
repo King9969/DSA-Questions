98f60f5df6524102ff777f80c0a19ee039fe6563
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-17 23:00:00 +0530

    Update Height of Binary Tree.cpp

--- Height of Binary Tree.cpp
+++ Height of Binary Tree.cpp
@@ -1,2 +1,4 @@
- if(node==NULL){return 0;}
-        return max(height(node->left),height(node->right))+1;
+ int ht(Node *root){
+        if(root==NULL)return 0;
+        return max(ht(root->left),ht(root->right))+1;
+    }

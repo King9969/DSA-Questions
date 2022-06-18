b2770287431e82f2abe9e6d9db18b212d79b3670
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-18 11:16:01 +0530

    Update Reverse Level Order Traversal.cpp

--- Reverse Level Order Traversal.cpp
+++ Reverse Level Order Traversal.cpp
@@ -1,17 +1,12 @@
-vector<int> reverseLevelOrder(Node *node)
-{
    vector<int>v;
      queue<Node*>q;
      q.push(node);
-     int n=q.size();
-     
-     while(!q.empty()){
-     for(int i=0;i<n;i++){
+    
+     while(!q.empty()){ 
          Node *top=q.front();
          q.pop();
          v.push_back(top->data);
          if(top->right!=NULL)q.push(top->right);
          if(top->left!=NULL)q.push(top->left);}
-        
-     } reverse(v.begin(),v.end());return v;
-}
+
+      reverse(v.begin(),v.end());return v;

2c8b337e00de2932d41e7e5cdeaad70fd8b8b38e
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-18 11:16:36 +0530

    Update level order traversal.cpp

--- level order traversal.cpp
+++ level order traversal.cpp
@@ -1,13 +1,12 @@
- vector<int>v;
+   vector<int>v;
      queue<Node*>q;
      q.push(node);
-     int n=q.size();
-     
-     while(!q.empty()){
-     for(int i=0;i<n;i++){
+    
+     while(!q.empty()){ 
          Node *top=q.front();
          q.pop();
          v.push_back(top->data);
-         if(top->left!=NULL)q.push(top->left);
-         if(top->right!=NULL)q.push(top->right);}
-     }return v;
+         if(top->right!=NULL)q.push(top->right);
+         if(top->left!=NULL)q.push(top->left);}
+
+      reverse(v.begin(),v.end());return v;

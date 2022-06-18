f807ce57a64b6e90891f72c6a529b756544d9920
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-18 10:50:30 +0530

    Create level order traversal.cpp

---
+++ level order traversal.cpp
@@ -0,0 +1,13 @@
+ vector<int>v;
+     queue<Node*>q;
+     q.push(node);
+     int n=q.size();
+     
+     while(!q.empty()){
+     for(int i=0;i<n;i++){
+         Node *top=q.front();
+         q.pop();
+         v.push_back(top->data);
+         if(top->left!=NULL)q.push(top->left);
+         if(top->right!=NULL)q.push(top->right);}
+     }return v;

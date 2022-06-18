4e8365181b3df0c3c69a306813cc1b72584eaad8
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-18 11:44:26 +0530

    Create zigzag order traversal.cpp

---
+++ zigzag order traversal.cpp
@@ -0,0 +1,17 @@
+ vector<int>v;
+     queue<Node*>q;
+     q.push(node);
+     int flag=true;
+     while(!q.empty()){
+         int n=q.size();
+        vector<int> temp(n);
+     for(int i=0;i<n;i++){
+         Node *top=q.front();
+         q.pop();
+         if(flag)temp[i]=top->data;
+         else temp[(n-i)-1]=top->data;
+         if(top->left!=NULL)q.push(top->left);
+         if(top->right!=NULL)q.push(top->right);}
+         flag=!flag;
+         for(auto i:temp)v.push_back(i);
+    }return v;

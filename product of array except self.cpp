2ca9bcf4c8fbed17cb2887eea36ec04e45e890f1
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-19 21:04:05 +0530

    Update product of array except self.cpp

--- product of array except self.cpp
+++ product of array except self.cpp
@@ -1,15 +1,16 @@
-       vector<long long int> v;
-       int pr=1;
-       for(int i=0;i<n;i++){
-           v.push_back(pr);
-           pr=pr*nums[i];
-       }
-    int x=1;
-       for(int i=n-1;i>=0;i--){
-           v[i]=v[i]*x;
-           x=x*nums[i];
-       }
-       return v;
+       vector<long long int> ans(n);
+       vector<long long int> left_Product(n);
+       vector<long long int> right_Product(n);
+       left_Product[0]=1;
+       for(int i=1;i<n;i++)
+           left_Product[i]=left_Product[i-1]*nums[i-1];
+       right_Product[n-1]=1;
+       for(int j=n-2;j>=0;j--)
+           right_Product[j]=right_Product[j+1]*nums[j+1];
+       for(int i=0;i<n;i++)
+           ans[i]=left_Product[i]*right_Product[i];
+       return ans;
+    }
 // o(n^2)
   vector<long long int>ans;
        for(int i=0;i<n;i++){

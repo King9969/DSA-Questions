875156188a8677d28dec32d912328ae107f9bad6
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-19 17:27:11 +0530

    Update DFS of Graph.cpp

--- DFS of Graph.cpp
+++ DFS of Graph.cpp
@@ -1,7 +1,8 @@
  void dfs(int v,vector<int>&vis,vector<int>adj[],vector<int>&ans){
      ans.push_back(v);
      vis[v]=1;
-     for(it:adj[v]){if(!vis[it])dfs(it,vis,adj,ans);}
+     for(int i=0;i<adj[v].size();i++){
+         if(!vis[adj[v][i]])dfs(adj[v][i],vis,adj,ans);}
  }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
          vector<int>ans;

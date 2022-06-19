55f394ffebb244d3fab38806348e76ac9a66f45c
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-19 23:01:03 +0530

    Create nCr.cpp

---
+++ nCr.cpp
@@ -0,0 +1,15 @@
+    int nCr(int n, int r){
+        if(r>n)return 0;
+       if(r>(n-r))r=n-r;
+       
+       int mod=1e9+7;
+       int dp[r+1];
+       memset(dp,0,sizeof(dp));
+       dp[0]=1;
+       for(int i=1;i<=n;i++){
+           for(int j=min(r,i);j>0;j--){
+               dp[j]=(dp[j-1]+dp[j])%mod;
+           }
+       }
+       return dp[r];
+    }

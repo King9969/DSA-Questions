ed1490982dcac2f1aca770849576bba0d838a438
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 23:26:34 +0530

    Update Maximum Index.cpp

--- Maximum Index.cpp
+++ Maximum Index.cpp
@@ -1,12 +1,15 @@
-int max_sum(int A[],int N){
-int sum=0,ans=INT_MIN,temp=0;
-for(int i=0;i<N;i++){
-    sum+=A[i];
-    temp+=A[i]*i;
-}
-ans=temp;
-for(int i=0;i<N-1;i++){
-    temp=temp-sum+A[i]*(N);
-    ans=max(ans,temp);
-}return ans;
-}
+    int maxIndexDiff(int arr[], int n) {
+       vector<int>left(n);
+       vector<int>right(n);
+       left[0]=arr[0];
+       for(int i=1;i<n;i++)left[i]=min(arr[i],left[i-1]);
+       right[n-1]=arr[n-1];
+       for(int i=n-2;i>=0;i--)right[i]=max(arr[i],right[i+1]);
+       int l=0,r=0,ans=0;
+       while(l<n&&r<n){
+           if(right[r]>=left[l]){
+               ans=max(ans,r-l);
+               r++;
+           }else l++;
+       }return ans;
+    }

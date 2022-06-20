e6eb1cc30602cf80e240cf0734fa0ad1bd83576e
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 11:53:23 +0530

    Update Left View of Binary Tree.cpp

--- Left View of Binary Tree.cpp
+++ Left View of Binary Tree.cpp
@@ -1,4 +1,23 @@
-   vector<int> v;
+       vector <int> bottomView(Node *root) {
+       int i=0;
+       queue<pair<Node*,int>>q;
+       map<int,int>m;
+       vector<int>ans;
+       q.push(make_pair(root,i));
+       while(q.size()>0){
+           Node *top=q.front().first;
+           i=q.front().second;
+           q.pop();
+           m[i]=top->data;
+           if(top->left)q.push(make_pair(top->left,i-1));
+           if(top->right)q.push(make_pair(top->right,i+1));
+       }
+       for (auto x:m)ans.push_back(x.second);
+       return ans;
+    }
+
+// recursion just 1 line change from top view
+vector<int> v;
    if(root==NULL)return v;
    queue<Node*> q;
    q.push(root);

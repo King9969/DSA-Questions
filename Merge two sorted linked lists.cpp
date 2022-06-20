747bdc2eb49e1cfd9475f5aa2da06f773ec7f99f
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 16:02:35 +0530

    Update Merge two sorted linked lists.cpp

--- Merge two sorted linked lists.cpp
+++ Merge two sorted linked lists.cpp
@@ -1,15 +1,17 @@
 Node* sortedMerge(Node* head1, Node* head2){  
-    if(head1->data>head2->data)
-     swap(head1,head2);
-     Node* head=head1;
+    Node* a=new Node(-1);
+    Node* tail=a;
     while(head1 && head2){
-        Node* prev=NULL;
-        while(head1&& head1->data<=head2->data){
-            prev=head1;
-            head1=head1->next;
-        }
-        prev->next=head2;
-        swap(head1,head2);
+        if(head1->data<head2->data){
+            tail->next=head1;
+            head1=head1->next;}
+        else{
+            tail->next=head2;
+            head2=head2->next;} 
+        tail=tail->next;
     }
-    return head;
-}
+    if(head1)tail->next=head1;
+    else if(head2)tail->next=head2;
+    
+    return a->next;
+}  

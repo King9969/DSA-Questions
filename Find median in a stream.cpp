087bdcf047c100d0cce1bf36a84e57d1cf45429d
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 19:10:52 +0530

    Create Find median in a stream.cpp

---
+++ Find median in a stream.cpp
@@ -0,0 +1,30 @@
+ priority_queue<int>maxheap;
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    void insertHeap(int &x)
+    {
+       if(maxheap.size()==0||maxheap.top()>=x) {maxheap.push(x);}
+       else {minheap.push(x);}
+       balanceHeaps();
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        if(maxheap.size()>minheap.size()+1){
+            minheap.push(maxheap.top());
+            maxheap.pop();
+        }
+        else if(minheap.size()>maxheap.size()){
+            maxheap.push(minheap.top());
+            minheap.pop();
+        }
+    }
+    
+    //Function to return Median.
+    double getMedian()
+    {
+        if(maxheap.size() == minheap.size()){
+            return ((double)maxheap.top() + (double)minheap.top()) / 2.0;}
+            
+        return (double)maxheap.top();
+    }

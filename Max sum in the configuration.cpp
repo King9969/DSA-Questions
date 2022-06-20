733f0f7400138022c9ebdf5c100795a78c0a9206
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 23:27:43 +0530

    Create Max sum in the configuration.cpp

---
+++ Max sum in the configuration.cpp
@@ -0,0 +1,11 @@
+int max_sum(int A[],int N){
+int sum=0,ans=INT_MIN,temp=0;
+for(int i=0;i<N;i++){
+    sum+=A[i];
+    temp+=A[i]*i;
+}
+ans=temp;
+for(int i=0;i<N-1;i++){
+    temp=temp-sum+A[i]*(N);
+    ans=max(ans,temp);
+}return ans;

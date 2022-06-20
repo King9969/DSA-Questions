1a999d9c00869a67b191c244c63bad44f34efc28
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-20 13:46:08 +0530

    Update Bottom View of Binary Tree.cpp

--- Bottom View of Binary Tree.cpp
+++ Bottom View of Binary Tree.cpp
@@ -1,3 +1,24 @@
+       vector <int> bottomView(Node *root) {
+       int i=0;
+       queue<pair<Node*,int>>q;
+       map<int,int>m;
+       vector<int>ans;
+       q.push(make_pair(root,i));
+       while(q.size()>0){
+           Node *top=q.front().first;
+           i=q.front().second;
+           q.pop();
+           m[i]=top->data;
+           if(top->left)q.push(make_pair(top->left,i-1));
+           if(top->right)q.push(make_pair(top->right,i+1));
+       }
+       for (auto x:m)ans.push_back(x.second);
+       return ans;
+    }
+
+
+//1 line change for top view
+
 void fillMap(Node* root, int d, int l,map<int, pair<int, int> >& m)
 {
     if (root == NULL) return;

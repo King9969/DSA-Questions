b7a2d8b9827cff4fbd077254255a7db669cea870
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-21 13:56:54 +0530

    Create Pascal Triangle.cpp

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,10 @@
+    vector<ll> nthRowOfPascalTriangle(int n) {
+        vector<ll>a{1,0};
+        for(int i=0;i<n-1;i++){
+            a.insert(a.begin(),0);
+            for(int j=0;j<a.size()-1;++j){
+                a[j]=(a[j]+a[j+1])&1000000007;
+            }
+        }   a.pop_back();
+            return a;
+    }

97736f863547b7b4f86ab2ff1f57900466d2f594
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-21 14:31:11 +0530

    Update Maximum Path Sum between 2 Leaf Nodes .cpp

--- Maximum Path Sum between 2 Leaf Nodes .cpp
+++ Maximum Path Sum between 2 Leaf Nodes .cpp
@@ -14,3 +14,20 @@
        if(root->left && root->right)return maxsum;
        return max(maxsum,x);
    }
+//test case wrong
+   int maxPath(Node* root,int &sum)
+   {
+       if(!root)return 0;
+       int l = maxPath(root->left,sum);
+       int r= maxPath(root->right,sum);
+       if(root->left==NULL)return r+root->data;
+       if(root->right==NULL)return l+root->data;
+        int cur=l+r+root->data;
+        sum=max(sum,cur);
+        return root->data+max(l,r);
+   }
+   int maxPathSum(Node* root){
+    int max=INT_MIN;
+    maxPath(root,max);
+    return max;
+   }

8bd239ea53e5a0e6706f41330a8058913018e62a
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-23 22:00:06 +0530

    Create Implement Stack using Linked List.cpp

---
+++ Implement Stack using Linked List.cpp
@@ -0,0 +1,17 @@
+//Function to push an integer into the stack.
+void MyStack ::push(int x) 
+{
+    StackNode *temp=new StackNode(x);
+    temp->next=top;
+    top=temp;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop() 
+{
+   if(top==NULL)
+   return -1;
+   int x=top->data;
+   top=top->next;
+   return x;
+}

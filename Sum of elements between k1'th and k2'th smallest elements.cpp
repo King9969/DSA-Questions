b941e8bcd526d92d9f70cc24f5a6d160fe091b20
King9969 <71065155+King9969@users.noreply.github.com>
2022-06-28 18:07:48 +0530

    Create Sum of elements between k1'th and k2'th smallest elements.cpp

---
+++ Sum of elements between k1'th and k2'th smallest elements.cpp
@@ -0,0 +1,8 @@
+    long long sumBetweenTwoKth( long long A[], long long N, long long K1, long long K2)
+    {
+        sort(A,A+N);
+        long long ans=0;
+        for(long long i=K1;i<K2-1;i++){
+            ans+=A[i];
+        }return ans;
+    }

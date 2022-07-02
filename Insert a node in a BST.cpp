b0062ec6777dc8fa4268840d4cc8bdaaf086a7b6
King9969 <71065155+King9969@users.noreply.github.com>
2022-07-02 17:39:10 +0530

    Update Insert a node in a BST.cpp

--- Insert a node in a BST.cpp
+++ Insert a node in a BST.cpp
@@ -1,6 +1,6 @@
 Node* insert(Node* root, int Key) {
       if (!root)return new Node(Key);
       if (Key < root->data)root->left = insert(root->left, Key);
-      else if (Key > root->data)root->right = insert(root->right, Key);
+      if (Key > root->data)root->right = insert(root->right, Key);
       return root;
 }

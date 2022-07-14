4b253921f3c8013fff016fde2378257533f121cf
King9969 <71065155+King9969@users.noreply.github.com>
2022-07-15 00:48:04 +0530

    Update Maximum Rectangular Area in a Histogram.cpp

--- Maximum Rectangular Area in a Histogram.cpp
+++ Maximum Rectangular Area in a Histogram.cpp
@@ -1,25 +1,23 @@
     long long getMaxArea(long long arr[], int n)
     {
-        vector<long long> left(n,-1),right(n,n);
-        stack<long long> st1,st2;
-       for (int i=0;i<n;i++){
-           while(!st1.empty() && arr[i]<arr[st1.top()]){
-               right[st1.top()]=i;
-               st1.pop();
-           }
-           st1.push(i);
-       }
-       for (int i=n-1;i>=0;i--){
-           while(!st2.empty() && arr[i]<arr[st2.top()]){
-               left[st2.top()]=i;
-               st2.pop();
-           }
-           st2.push(i);
-       }
-       long long ans=0;
-       for (int i=0;i<n;i++){
-           int len=right[i]-1-(left[i]+1)+1;
-           ans=max(ans,len*arr[i]);
-       }
-       return ans;
+      stack < long long > st;
+       vector<long long> leftsmall(n,-1),rightsmall(n,n);
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && arr[st.top()] >= arr[i])st.pop();
+        if (st.empty())leftsmall[i] = 0;
+        else leftsmall[i] = st.top() + 1;
+        st.push(i);
+      }
+      while (!st.empty())st.pop();
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && arr[st.top()] >= arr[i])st.pop();
+        if (st.empty())rightsmall[i] = n - 1;
+        else rightsmall[i] = st.top() - 1;
+        st.push(i);
+      }
+      long long int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, arr[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;
     }

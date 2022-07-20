64b161abf6cde5891d56c7e393c34b2bc845d6fc
King9969 <71065155+King9969@users.noreply.github.com>
2022-07-21 03:31:56 +0530

    Create Pythagorean Triplet.cpp

---
+++ Pythagorean Triplet.cpp
@@ -0,0 +1,10 @@
+	bool checkTriplet(int arr[], int n) {
+	  for(int i=0;i<n;i++)arr[i]=arr[i]*arr[i];
+	  sort(arr,arr+n);
+	 for(int i=n-1;i>=2;i--)
+	  {int l=0,r=i-1;
+	      while(l<r)
+	      {	   if(arr[l]+arr[r]==arr[i])return true;
+	          arr[l]+arr[r]>arr[i]?r--:l++;  }   }
+	  return false;
+	}

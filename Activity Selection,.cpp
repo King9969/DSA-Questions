742a6bc835bf1bbad0027655ee0efbbe6e97719d
Rohit yadav <71065155+King9969@users.noreply.github.com>
2023-06-07 04:39:43 +0530

    Update Activity Selection,.cpp

--- Activity Selection,.cpp
+++ Activity Selection,.cpp
@@ -1,15 +1,18 @@
-    int activitySelection(vector<int> start, vector<int> end, int n)
-    {
-       vector<pair<int,int>>v;
-      for(int i  = 0 ; i < n;i++){
-          v.push_back({end[i],start[i]});}
-      sort(v.begin(),v.end());
-      int ans = 1 ;
-      int x = v[0].first;
-      for(int i = 1 ; i < n;i++){
-          if(v[i].second>x){
-              ans++;
-              x=v[i].first;
-          }}
-      return ans;
-    }
+int activitySelection(vector<int> start, vector<int> end, int n)
+{
+   vector<pair<int,int>> v;
+   for(int i = 0; i < n; i++){
+      v.push_back({end[i], start[i]});
+   }
+   sort(v.begin(), v.end());
+   int ans = 1;
+   int x = v[0].first;
+   for(int i = 1; i < n; i++){
+      if(v[i].second > x){
+         ans++;
+         x = v[i].first;
+      }
+   }
+   return ans;
+}
+
